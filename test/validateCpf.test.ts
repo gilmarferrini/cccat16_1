627f4cdd9d1a1ebce17378eef48c86f930078b0e
gilmarferrini <ferrinigilmar@gmail.com>
2024-08-10 10:22:13 -0300

    refactor: validate cpf

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,13 @@
+import { validate } from "../src/validateCpf";
+
+const validsCpfs = ["97456321558", "71428793860", "87748248800"];
+test.each(validsCpfs)("Deve testar um cpf válido: %s", (cpf: string) => {
+  expect(validate(cpf)).toBe(true);
+});
+
+test.each([undefined, null, "11111111111", "123", "1234566789123456789"])(
+  "Deve testar um cpf inválido: %s",
+  function (cpf: any) {
+    expect(validate(cpf)).toBe(false);
+  },
+);
